c882a1c2649b4e66be8bce607dd6b03696c10742
nason mangeli <kuuchman@gmail.com>
2017-08-24 08:17:33 +0300

    Make subscribe form active create overlay  Add overlay, open and close button and menu list  Add heading tag for subcription acknoledgement  Add background color for overlay.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
-$(document).ready(function () {
-  $("#clickme").click(function () {
-    $(".navbar-menu").toggle("slow");
-  });
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
+var resetField= function() {
+  $('#email').val("");
+}
+
+
+
+
+$(document).ready(function() {
+  $('.form-inline').submit(function(event) {
+    var email = $('#email').val();
+    $('.subdetails').text(email + " has been successfully added to our email list. Thank you.");
+    resetField();
+    event.preventDefault();
+  })
 });
